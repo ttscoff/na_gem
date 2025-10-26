d4153ced5e52c0c534f4425a12df86dfe4ae0e7a
Brett Terpstra <me@brettterpstra.com>
2025-10-26 08:44:06 -0500

    ### 1.2.85
    2025-10-26 08:44
    
    #### IMPROVED
    
    - YARD documentation
    - Currently at 100% YARD documentation
    
    #### FIXED
    
    - Nil handler for trunc_middle
    - Nil handler for hihglight_file

--- lib/na/version.rb
+++ lib/na/version.rb
@@ -2,9 +2,6 @@
 
 ##
 # Main module for the na gem, providing version information.
-#
-# @example Get the current version
-#   Na::VERSION #=> '1.2.85'
 module Na
   ##
   # Current version of the na gem.
